f8e1fa16b8e3ef9d04ddadc8b8c91afc3bbc3535
Roc Vallès Domènech <vallesroc@gmail.com>
2021-03-29 16:34:12 +0900

    RomUtils: Header signature actually tells us the kickstart size.

--- RomUtils.py
+++ RomUtils.py
@@ -1,5 +1,10 @@
 class RomUtils(object):
-    romsig = [0x11114ef9, 0x11144ef9]
+    ROMSIG512K = 0x11144ef9
+    ROMSIG256K = 0x11114ef9
+    romsig = {
+        0x11114ef9: 256*1024,
+        0x11144ef9: 512*1024
+    }
     def __init__(self, **kwargs):
         amiga = kwargs["debugger"]
         self.amiga = amiga
@@ -7,26 +12,19 @@ class RomUtils(object):
         romhdr = amiga.peek32(romaddr)
         romver = amiga.peek16(romaddr+12)
         romrev = amiga.peek16(romaddr+14)
-        print(f"addr: {hex(romaddr)} romhdr: {hex(romhdr)} version: {romver}.{romrev}")
-        if not romhdr in self.romsig:
-            romhdr = amiga.peek32(romaddr)
+        print(f"addr: {hex(romaddr)}, romhdr: {hex(romhdr)}, version: {romver}.{romrev}")
+        if (not romhdr in self.romsig) or (self.romsig[romhdr] == 256*1024):
             romaddr = 0xfc0000
+            romhdr = amiga.peek32(romaddr)
             romver = amiga.peek16(romaddr+12)
             romrev = amiga.peek16(romaddr+14)
-            print(f"addr: {hex(romaddr)} romhdr: {hex(romhdr)} version: {romver}.{romrev}")
+            print(f"addr: {hex(romaddr)}, romhdr: {hex(romhdr)}, version: {romver}.{romrev}")
+        romsize = self.romsig[romhdr]
+        print(f"size: {hex(romsize)}")
+        if (romaddr + romsize) > 0x1000000:
+            raise ValueError("ROM continues past 24bit address space?!")
         self.ver = romver
         self.rev = romrev
-        if romver > 36:
-            romsize = 512*1024
-        elif romver < 36:
-            romsize = 256*1024
-        else:
-            if romrev > 15:
-                romsize = 512*1024
-            else:
-                romsize = 256*1024
-        if romsize == 256*1024:
-            romaddr = 0xfc0000
         self.addr = romaddr
         self.size = romsize
         return
